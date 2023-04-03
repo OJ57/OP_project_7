4cc6f78319039b22562633e77a36f5edd8d6ef7d
OJ57 <arturo.bandini2@protonmail.com>
2023-04-03 16:48:45 +0200

    Dernière version RAS

--- API_script.py
+++ API_script.py
@@ -5,12 +5,13 @@ import pandas as pd
 
 # Load the pre-processed data
 data = pd.read_csv("test_API.csv")  # Replace with the path to your CSV file
-# data = data.set_index("client_id")  # Assuming the column name is "client_id"
 
 # Load the model
 model = joblib.load("model_prediction.joblib")
 
 app = FastAPI()
+
+THRESHOLD = 0.52
 
 
 class ClientID(BaseModel):
@@ -21,15 +22,16 @@ class ClientID(BaseModel):
 async def predict_failure(client: ClientID):
     client_id = client.client_id
 
-    if client_id not in data.index:
-        print(client_id)
+    if client_id not in data['SK_ID_CURR'].values:
         raise HTTPException(status_code=404, detail=f"Client ID {client_id} not found")
 
     # Get the features for the given client_id
-    features = data.loc[client_id].values.reshape(1, -1)
+    features = data.loc[data['SK_ID_CURR'] == client_id].drop(columns='SK_ID_CURR').values.reshape(1, -1)
 
     # Make a prediction using the loaded model
-    prediction = model.predict_proba(features)[:, 1]
+    prediction = model.predict_proba(features)[:, 1][0]
+
+    will_fail = 'yes' if int(prediction >= THRESHOLD) == 1 else 'no'
 
     # Return the prediction
-    return {"client_id": client_id, "probability_of_failure": prediction[0]}
+    return {"client_id": client_id, "probability_of_failure": prediction, "will_fail": will_fail}
