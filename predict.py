de878f8016ea4258499431680e5e4a26f9fbb2a0
OJ57 <arturo.bandini2@protonmail.com>
2023-04-04 17:24:52 +0200

    Correction du Procfile

--- predict.py
+++ predict.py
@@ -1,6 +1,5 @@
 import requests
 
-# Replace the URL with your API's URL if it's different
 url = "http://127.0.0.1:8000/predict"
 
 # Replace this with the ID number for which you want to make a prediction
---
+++ predict_heroku.py
@@ -0,0 +1,15 @@
+import httpx
+
+API_URL = "https://fastapi-project7.herokuapp.com"
+
+client_id = 181733
+
+# Send a POST request with the client_id as input
+response = httpx.post(f"{API_URL}/predict", json={"client_id": client_id})
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    prediction_result = response.json()
+    print("Prediction result:", prediction_result)
+else:
+    print("Error:", response.status_code, response.text)
