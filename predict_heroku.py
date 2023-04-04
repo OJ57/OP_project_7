6291c76f574b32771083a77a323d03b391bdea79
OJ57 <arturo.bandini2@protonmail.com>
2023-04-04 17:26:20 +0200

    Ajout d'un script pour tester Heroku

--- predict_heroku.py
+++ predict_heroku.py
@@ -9,7 +9,10 @@ response = httpx.post(f"{API_URL}/predict", json={"client_id": client_id})
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
+
     prediction_result = response.json()
     print("Prediction result:", prediction_result)
+
 else:
+
     print("Error:", response.status_code, response.text)
